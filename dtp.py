ab29581f4c2c8adbbe28dc70638c4ca42eb7034f
Adam Tokarski <adrestokarskiego@gmail.com>
2015-05-13 22:03:08 +0200

    Update dtp.py
    Regex was missing polish chars, ie, word "żółci" was treated as short word, that was needing to add non-breaking space.

--- dtp.py
+++ dtp.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from django import template;
 import re;
 
@@ -7,7 +8,8 @@ register = template.Library();
 @register.filter(is_safe=True)
 def removeOrphans(text):
 	# reduces the occurence of Orphans (in Polish notation, see http://pl.wikipedia.org/wiki/Sierota_(DTP))
-	return re.sub(r'\b([^ ]{1,2})\b ', r'\1&nbsp;', text)
+	r = re.compile(r'\b([^ ]{1,2})\b[ \n]', re.UNICODE)
+	return r.sub(r'\1&nbsp;', text)
 	
 	#http://pl.wikipedia.org/wiki/Sierota_(DTP)	
 	#http://pl.wikipedia.org/wiki/Wdowa_(DTP)
@@ -15,3 +17,4 @@ def removeOrphans(text):
 	
 	#todo: dodać przypadki 3literowe (?), np. gdy króciutkim wyrazem zaczyna się nowe zdanie, lepiej, by nie było na końcu wiersza, etc.
 	
+	#https://docs.djangoproject.com/en/1.8/howto/custom-template-tags/
