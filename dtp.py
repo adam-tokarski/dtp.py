0d1445b08515fb7fd796bab5a17201f13addbf8f
Adam Tokarski <adrestokarskiego@gmail.com>
2015-05-13 23:31:42 +0200

    Update dtp.py
    Added rule to avoid alone short words of new paragraph.

--- dtp.py
+++ dtp.py
@@ -8,13 +8,14 @@ register = template.Library();
 @register.filter(is_safe=True)
 def removeOrphans(text):
 	# reduces the occurence of Orphans (in Polish notation, see http://pl.wikipedia.org/wiki/Sierota_(DTP))
-	r = re.compile(r'\b([^ ]{1,2})\b[ \n]', re.UNICODE)
+	r = re.compile(r'([^<]\b[^ ]{1,2})\b[ \n]', re.UNICODE)
+	text = r.sub(r'\1&nbsp;', text)
+
+	# to avoid alone short words of new paragraph
+	r = re.compile(r"(\.[ \n]?[^ \n]{1,4})[ \n]")
 	return r.sub(r'\1&nbsp;', text)
 	
 	#http://pl.wikipedia.org/wiki/Sierota_(DTP)	
 	#http://pl.wikipedia.org/wiki/Wdowa_(DTP)
 	#http://pl.wikipedia.org/wiki/Szewc_(DTP)
 	
-	#todo: dodać przypadki 3literowe (?), np. gdy króciutkim wyrazem zaczyna się nowe zdanie, lepiej, by nie było na końcu wiersza, etc.
-	
-	#https://docs.djangoproject.com/en/1.8/howto/custom-template-tags/
